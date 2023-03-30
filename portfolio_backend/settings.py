15cf083b85c538d1c3d8eb0c90a130142ec3e4a9
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-30 21:17:19 +0200

    host database on s3

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_s3_storage',
+    'django_s3_sqlite',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,10 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django_s3_sqlite",
+        "NAME": "db.sqlite3",
+        "BUCKET": "django-portfolio-sqlite",
     }
 }
 
