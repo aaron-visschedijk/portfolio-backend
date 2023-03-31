d8b6ccff08931baaa40406ca9ab82fdfb2b5d636
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-31 04:37:36 +0200

    add project model

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['4i3qvu8esj.execute-api.eu-central-1.amazonaws.com', 'localhost
 # Application definition
 
 INSTALLED_APPS = [
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
