f654cb5146d5124ecf5f44f3cdf90782d138931e
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-04-03 16:16:06 +0200

    Update about view

--- about/views.py
+++ about/views.py
@@ -1,10 +1,22 @@
+import json
 from django.http import HttpResponse
 from django.core import serializers
 
 from about.models import Bio, Photo
+from django.core.exceptions import ObjectDoesNotExist
 
 def active(request):
-    bio = Bio.objects.get(active=True)
-    photo = Photo.objects.get(active=True)
-    json = serializers.serialize('json', [bio, photo])
-    return HttpResponse(json, content_type='application/json')
+    response = {}
+    try:
+        bio = Bio.objects.get(active=True)
+        response['content'] = bio.content
+    except ObjectDoesNotExist:
+        HttpResponse(status=404)
+    
+    try:
+        photo = Photo.objects.get(active=True)
+        response['photo'] = photo.image.name
+    except ObjectDoesNotExist:
+        HttpResponse(status=404)
+
+    return HttpResponse(json.dumps(response), content_type='application/json')
