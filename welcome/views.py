fb9130e4b621c8586b6c64532e753e8726824e77
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-04-04 06:40:34 +0200

    change api root to a slightly better message

--- welcome/views.py
+++ welcome/views.py
@@ -1,6 +1,7 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("I'm Aaron Visschedijk, software developer")
+    return HttpResponse(json.dumps("The portfolio backend api is live!"))
